61d7d77a4056752f8f8a900676f8b92a050f67fc
TonySpano <67001237+TonySpano@users.noreply.github.com>
2023-01-29 08:53:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import streamlit as st
+
+st.title('Test Page')
+
+st.subheader('Description')
+st.write('testing')
